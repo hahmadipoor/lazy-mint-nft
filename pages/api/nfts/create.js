6ee9151d566b371ff2ed62f8a56bfd6405598908
hossein <ahmadipoor.h@gmail.com>
2024-12-02 12:30:50 +0330

    modifyint dependency versions

--- pages/api/nfts/create.js
+++ pages/api/nfts/create.js
@@ -11,19 +11,17 @@ const client = new MongoClient(uri, {
 })
 
 export default async function handler(req, res) {
+
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' })
-  }
-
+  }  
   try {
     const { name, description, price, imageUrl, owner } = req.body
-
-    await client.connect()
+    await client.connect();
     await client
       .db('dapplazy')
       .collection('nfts')
       .insertOne({ name, description, price, imageUrl, owner })
-
     return res.status(200).json({ message: 'NFT created successfully' })
   } catch (error) {
     console.error('Error creating NFT:', error)
